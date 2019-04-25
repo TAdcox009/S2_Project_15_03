058311b7e3212df2fd23edbae1cfe8ea7dc0f188
Unknown <TAdcox009@west-mec.org>
2019-04-24 18:28:40 -0700

    commit 2
    Created an onblur event handler for different input boxes.

--- mas_register.js
+++ mas_register.js
@@ -29,20 +29,25 @@
 */
 
 // 4. Return to the mas_register.js file in your editor. Directly below the initial comment section, insert an event listener for the window load event. Run an anonymous function in response to the event containing the following commands:
-window.addEventListener("load", function() {
-// a. Call the calcCart() function (which you will create shortly).
-calcCart();
+window.addEventListener("load", function () {
+  // a. Call the calcCart() function (which you will create shortly).
+  calcCart();
 
-// b. Create an onclick event handler for the regSubmit button that runs the sessionTest() function when the button is clicked.
-document.getElementById("regSubmit").onclick = sessionTest;
+  // b. Create an onclick event handler for the regSubmit button that runs the sessionTest() function when the button is clicked.
+  document.getElementById("regSubmit").onclick = sessionTest;
 
-// c. Create onblur event handlers for the input boxes with the IDs: fnBox, lnBox, groupBox, mailBox, phoneBox, and banquetBox, running the calcCart() function in response to each event.
+  // c. Create onblur event handlers for the input boxes with the IDs: fnBox, lnBox, groupBox, mailBox, phoneBox, and banquetBox, running the calcCart() function in response to each event.
+  document.getElementById("fnBox").onblur = calcCart;
+  document.getElementById("lnBox").onblur = calcCart;
+  document.getElementById("groupBox").onblur = calcCart;
+  document.getElementById("mailBox").onblur = calcCart;
+  document.getElementById("phoneBox").onblur = calcCart;
+  document.getElementById("banquetBox").onblur = calcCart;
+
+  // d. Create an onchange event handler for the sessionBox selection list, running the calcCart() function when the selection list is changed.
 
 
-// d. Create an onchange event handler for the sessionBox selection list, running the calcCart() function when the selection list is changed.
-
-
-// e. Create an onclick event handler for the mediaCB check box, running the calcCart() function in response.
+  // e. Create an onclick event handler for the mediaCB check box, running the calcCart() function in response.
 
 
 });
